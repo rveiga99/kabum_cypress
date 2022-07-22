aa6272c75b5a675c7aca2e3bfe97a522b80260bb
rveiga99 <veigapaiva@hotmail.com>
2022-07-22 12:35:23 -0300

    Atualizando cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "u928gc",
     viewportWidth: 1440,
     viewportHeight: 720,
-    baseUrl: 'https://www.kabum.com.br',
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
