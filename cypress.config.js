35d5a6c5e931a1c2215f53a8f268480bf8e2c07b
rveiga99 <veigapaiva@hotmail.com>
2022-07-22 14:33:33 -0300

    Ajuste

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     projectId: "u928gc",
+    baseUrl:"https://www.kabum.com.br",
     viewportWidth: 1440,
     viewportHeight: 720,
 
--- cypress/pages/home.js
+++ cypress/pages/home.js
@@ -8,7 +8,7 @@ class home {
     }
 
     go_main_page(){
-        cy.visit('https://www.kabum.com.br')
+        cy.visit('/')
         cy.get('.IconLogoKabum')
         cy.get('#linkLoginHeader').should('have.text','Login')
     }
